9fbedd5ef84d33c235b11bf8fae374324d3a7c67
max <maxhthomas@outlook.com>
2021-03-15 00:23:15 +0000

    fixed

--- bot.js
+++ bot.js
@@ -14,7 +14,8 @@ async function fetchStats(user) {
    let player = await r6api.createPlayer(user, platform, session).catch(e => { console.error(e) }); 
    let stats = await r6api.getStatsBySeason(player, session, season).catch(e => { console.error(e) });
    return stats
-} 
+  }
+
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -22,20 +23,31 @@ client.on("ready", () => {
  
 client.on("message", async (message) => {
   if (message.content.startsWith("!find")) {
-    let user_raw = message.content
+    const user_raw = message.content
     var user = user_raw.replace("!find ","")
-    let stats = await fetchStats(String(user)).catch(e => { console.error(e) });
-    const username = user.charAt(0).toUpperCase() + user.slice(1)
-    const statsEmbed = new Discord.MessageEmbed()
-        .setColor('#B980ca')
-        .setTitle('Ranked Stats')
-        .setDescription(username)
-        .addFields(
-            { name: 'mmr', value: String(stats.map(o  => o.mmr)), inline: true },
-            { name: 'max mmr', value: String(stats.map(o  => o.max_mmr)), inline: true })
-        .setTimestamp()
-        .setFooter('R6 Slut');
-    message.channel.send(statsEmbed);
+    if (/^([a-zA-Z0-9|._-]){3,15}$/.test(user)) {
+      try {
+        let stats = await fetchStats(String(user));
+        const username = user.charAt(0).toUpperCase() + user.slice(1)
+        const statsEmbed = new Discord.MessageEmbed()
+          .setColor('#B980ca')
+          .setTitle('Ranked Stats')
+          .setDescription(username)
+          .addFields(
+              { name: 'mmr', value: String(stats.map(o  => o.mmr)), inline: true },
+              { name: 'max mmr', value: String(stats.map(o  => o.max_mmr)), inline: true })
+          .setTimestamp()
+          .setFooter('R6 Slut');
+        message.channel.send(statsEmbed);
+      } catch {
+        const statsEmbed = new Discord.MessageEmbed()
+          .setColor('#B980ca')
+          .setTitle('User not found')
+          .setTimestamp()
+          .setFooter('R6 Slut');
+        message.channel.send(statsEmbed);
+      }
+      } 
     } 
 });
  
