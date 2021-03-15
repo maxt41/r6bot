4e0271c3e3a8870e45a854b35f3c9f5034a8d0e9
max <maxhthomas@outlook.com>
2021-03-15 16:21:48 +0000

    ranks

--- bot.js
+++ bot.js
@@ -26,6 +26,80 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
  
+function getRank(mmr) {
+  if (mmr >= 5000) {
+    return rank = "Champion"
+  }
+  if (mmr >= 4400) {
+    return rank = "Diamond"
+  } 
+  if (mmr >= 4000) {
+    return rank = "Platinum I"
+  }
+  if (mmr >= 3600) {
+    return rank = "Platinum II"
+  } 
+  if (mmr >= 3200) {
+    return rank = "Platinum III"
+  } 
+  if (mmr >= 3000) {
+    return rank = "Gold I"
+  } 
+  if (mmr >= 2800) {
+    return rank = "Gold II"
+  } 
+  if (mmr >= 2600) {
+    return rank = "Gold III"
+  } 
+  if (mmr >= 2500) {
+    return rank = "Silver I"
+  } 
+  if (mmr >= 2400) {
+    return rank = "Silver II"
+  } 
+  if (mmr >= 2300) {
+    return rank = "Silver III"
+  } 
+  if (mmr >= 2200) {
+    return rank = "Silver IV"
+  } 
+  if (mmr >= 2100) {
+    return rank = "Silver V"
+  } 
+  if (mmr >= 2000) {
+    return rank = "Bronze I"
+  } 
+  if (mmr >= 1900) {
+    return rank = "Bronze II"
+  }
+  if (mmr >= 1800) {
+    return rank = "Bronze III"
+  } 
+  if (mmr >= 1700) {
+    return rank = "Bronze IV"
+  } 
+  if (mmr >= 1600) {
+    return rank = "Bronze V"
+  } 
+  if (mmr >= 1500) {
+    return rank = "Copper I"
+  }
+  if (mmr >= 1400) {
+    return rank = "Copper II"
+  }
+  if (mmr >= 1300) {
+    return rank = "Copper III"
+  }
+  if (mmr >= 1200) {
+    return rank = "Copper IV"
+  }
+  if (mmr >= 1100) {
+    return rank = "Copper V"
+  } else {
+    return rank = "Dust I"
+  }
+}
+
 client.on("message", async (message) => {
   if (message.content.startsWith("!find")) {
     const user_raw = message.content
@@ -33,11 +107,13 @@ client.on("message", async (message) => {
     if (/^([a-zA-Z0-9|._-]){3,15}$/.test(user)) {
       try {
         let stats = await fetchStats(String(user));
+        const mmr = stats.map(o  => o.mmr)
+        const rank = getRank(mmr)
         const username = user.charAt(0).toUpperCase() + user.slice(1)
         const statsEmbed = new Discord.MessageEmbed()
           .setColor('#B980ca')
-          .setTitle('Ranked Stats')
-          .setDescription(username)
+          .setTitle(username)
+          .setDescription(rank)
           .addFields(
               { name: 'mmr', value: String(stats.map(o  => o.mmr)), inline: true },
               { name: 'max mmr', value: String(stats.map(o  => o.max_mmr)), inline: true })
